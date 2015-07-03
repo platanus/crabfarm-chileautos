a49d362ee54bd7eaffddc808b5c556d382829901
Amós Rivera <arivera.utpmp@gmail.com>
2015-07-03 13:59:30 -0300

    fix(): use crabfarm struct

--- app/reducers/car_list_reducer.rb
+++ app/reducers/car_list_reducer.rb
@@ -11,18 +11,20 @@ class CarListReducer < Crabfarm::BaseReducer
     @car_list ||= css('#results tr[onclick^="location.href"]')
   end
 
-  def parse_car(car)
+  def parse_car(car_html)
 
-    OpenStruct.new({
-      image: car.at_css("img").attr("src"),
-      model: car.at_css("td:eq(2) a").text,
-      id: get_id(car.at_css("td:eq(2) a").attr("href")),
-      year: car.at_css("td:eq(3)").text,
-      price: car.at_css("td:eq(4)").text,
-      seller: car.at_css("td:eq(5)").text,
-      city: get_city(car.at_css("td:eq(6)").text),
-      published: get_published(car.at_css("td:eq(6)").text)
-    })
+    car = Car.new
+
+    car.image = car_html.at_css("img").attr("src")
+    car.model = car_html.at_css("td:eq(2) a").text
+    car.id = get_id(car_html.at_css("td:eq(2) a").attr("href"))
+    car.year = car_html.at_css("td:eq(3)").text
+    car.price = car_html.at_css("td:eq(4)").text
+    car.seller = car_html.at_css("td:eq(5)").text
+    car.city = get_city(car_html.at_css("td:eq(6)").text)
+    car.published = get_published(car_html.at_css("td:eq(6)").text)
+
+    car
   end
 
   def get_id(url)
