f19f58376f6fc08964b0a5a8593b79c7903018ef
Amós Rivera <arivera.utpmp@gmail.com>
2015-07-29 16:49:32 -0300

    refactor(): remove unnecessary code

--- app/reducers/car_list_reducer.rb
+++ app/reducers/car_list_reducer.rb
@@ -1,4 +1,3 @@
-require "ostruct"
 class CarListReducer < Crabfarm::BaseReducer
 
   has_field :cars
@@ -13,16 +12,16 @@ class CarListReducer < Crabfarm::BaseReducer
 
   def parse_car(car_html)
 
-    car = Car.new
-
-    car.image = car_html.at_css("img").attr("src")
-    car.model = car_html.at_css("td:eq(2) a").text
-    car.id = get_id(car_html.at_css("td:eq(2) a").attr("href"))
-    car.year = car_html.at_css("td:eq(3)").text
-    car.price = car_html.at_css("td:eq(4)").text
-    car.seller = car_html.at_css("td:eq(5)").text
-    car.city = get_city(car_html.at_css("td:eq(6)").text)
-    car.published = get_published(car_html.at_css("td:eq(6)").text)
+    car = {
+      image: car_html.at_css("img").attr("src"),
+      model: car_html.at_css("td:eq(2) a").text,
+      id: get_id(car_html.at_css("td:eq(2) a").attr("href")),
+      year: car_html.at_css("td:eq(3)").text,
+      price: car_html.at_css("td:eq(4)").text,
+      seller: car_html.at_css("td:eq(5)").text,
+      city: get_city(car_html.at_css("td:eq(6)").text),
+      published: get_published(car_html.at_css("td:eq(6)").text)
+    }
 
     car
   end
